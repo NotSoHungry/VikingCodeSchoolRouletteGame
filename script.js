d3a9852abe6981a48b43afaf2ee065345887b62f
Pawel Bielinski <pawbielinski@gmail.com>
2018-03-22 22:56:09 +0100

    Probably final version

--- script.js
+++ script.js
@@ -4,34 +4,51 @@ function Roulette (playerName, playerMoney) {
     this._currentBet = null;
     this._currentSpin = null;
     
-    this._specialNumbers = ["0", "00"];
-    this._normalNumbers = (function() {
+    this._spinNumbers = (function() {
         
         function isNumEvenOrOdd (number) {
-            return (number % 2 === 0) ? "even" : "odd";
+            if (number === "0" || number === "00") {
+                return "Special"
+            } else {
+                return (number % 2 === 0) ? "Even" : "Odd";
+            }
         }
         function whichColor (number) {
-            var isNumberRed = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36].filter((element) => {
-                return element === number;
-            })
+            if (number === "0" || number === "00") {
+                return "Special"
+            } else {
+                var isNumberRed = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36].filter((element) => {
+                    return element === number;
+                })
         
-            return (isNumberRed[0]) ? "red" : "black";
+                return (isNumberRed[0]) ? "Red" : "Black";
+            }
         }
         function belongsToWhichHalf (number) {
-            return (number < 19) ? "first" : "second";
+            if (number === "0" || number === "00") {
+                return "Special"
+            } else {
+                return (number < 19) ? "firstHalf" : "secondHalf";
+            }
         }
         function belongsToWhichTwelve (number) {
-            return (number < 13) ? "first" :
-                   (number < 25) ? "second" : "third";
+            if (number === "0" || number === "00") {
+                return "Special"
+            } else {
+                return (number < 13) ? "firstTwelve" :
+                       (number < 25) ? "secondTwelve" : "thirdTwelve";
+            }
         }
         var arr = [];
     
-        for (i = 1; i <= 36; i++) {
-            arr.push(i);
+        for (i = -1; i <= 36; i++) {
+            let varToPush = (i === -1) ? "00" :
+                            (i === 0) ? "0" : i;
+            arr.push(varToPush);
         }   
         return arr.map((element, index, array) => {
             return {
-                number: array[index],
+                number: array[index].toString(),
                 evenOrOdd: isNumEvenOrOdd(element),
                 color: whichColor(element),
                 whichHalf: belongsToWhichHalf(element),
@@ -47,11 +64,8 @@ Roulette.prototype = {
     get player () {
         return this._player;
     },
-    get specialNumbers () {
-        return this._specialNumbers;
-    },
-    get normalNumbers () {
-        return this._normalNumbers;
+    get spinNumbers () {
+        return this._spinNumbers;
     },
     get moneyAmount () {
         return this._moneyAmount;
@@ -61,7 +75,7 @@ Roulette.prototype = {
     }
 }
 
-Roulette.prototype.winBet = function (money, checkedValue) {
+Roulette.prototype.winBet = function (money, ckeckedValue) {
     this.moneyAmount += money;
     console.log(`Your bet: ${this._currentBet}, Current spin: ${checkedValue} - you've won $${money}`);
 }
@@ -72,28 +86,53 @@ Roulette.prototype.loseBet = function (money, checkedValue) {
 }
 
 Roulette.prototype.spin = function () {
-    let spinResult = Math.floor(Math.random() * 38);
+    let spinResult = Math.floor(Math.random() * 37);
     
-    return (spinResult === 0) ? this._specialNumbers[0] :
-           (spinResult === 37) ? this._specialNumbers[1] : this._normalNumbers[spinResult - 1];
+    return (spinResult === 0) ? this._spinNumbers[0] :
+           (spinResult === 1) ? this._spinNumbers[1] : this._spinNumbers[spinResult];
 }
 
 Roulette.prototype.bet = function (userBet, betMoney) {
     this._currentSpin = this.spin();
-    this._currentBet = userBet;
+    console.log(this._currentSpin);
+    this._currentBet = userBet.toString();
 
-    let checkedValue = null;
+    let checkedValue = null,
+        prizeMultiplier = null;
 
     switch (true) {
+        case (userBet == "0"):
+            checkedValue = this._currentSpin.number;
+            priceMultiplier = 35;
+            break;
+        case (userBet == "00"):
+            checkedValue = this._currentSpin.number;
+            priceMultiplier = 35;
+            break;
         case (userBet > 0 && userBet < 36):
             checkedValue = this._currentSpin.number;
-            (userBet == checkedValue) ? this.winBet(betMoney * 35, checkedValue) : this.loseBet(betMoney, checkedValue);
+            priceMultiplier = 35;
             break;
         case (userBet.toLowerCase() == "odd" || userBet.toLowerCase() == "even"):
-        checkedValue = this._currentSpin.evenOrOdd;
-        (userBet.toLowerCase() == checkedValue) ? this.winBet(betMoney, checkedValue) : this.loseBet(betMoney, checkedValue);
-        break;
+            checkedValue = this._currentSpin.evenOrOdd;
+            priceMultiplier = 1;
+            break;
+        case (userBet.toLowerCase() == "red" || userBet.toLowerCase() == "black"):
+            checkedValue = this._currentSpin.color;
+            priceMultiplier = 1;
+            break;
+        case (userBet.toLowerCase() == "firsthalf" || userBet.toLowerCase() == "secondhalf"):
+            checkedValue = this._currentSpin.whichHalf;
+            priceMultiplier = 1;
+            break;
+        case (userBet.toLowerCase() == "firsttwelve" || userBet.toLowerCase() == "secondtwelve" || userBet.toLowerCase() == "thirdtwelve" ):
+            checkedValue = this._currentSpin.whichTwelve;
+            priceMultiplier = 1;
+            break;
     }
+
+
+        (String(userBet).toLowerCase() == String(checkedValue).toLowerCase()) ? this.winBet(betMoney * priceMultiplier, checkedValue) : this.loseBet(betMoney, checkedValue);
 }
 
 Roulette.prototype.buyIn = function (money) {
@@ -102,6 +141,10 @@ Roulette.prototype.buyIn = function (money) {
    } else {
        return "You cannot take this!";
    }
+}
+
+Roulette.prototype.bankRoll = function () {
+    return this.moneyAmount;
 }
 
 let game = new Roulette("Pawel", 1000);
